5877249e54e3ac3b1544b02d4cbed2bf08ba6dd0
vcinquini <vcinquini@gmail.com>
2024-03-20 09:10:00 -0300

    update to net 8.0

--- OpenWeatherMapIoCv2/OpenMapApi.cs
+++ OpenWeatherMapIoCv2/OpenMapApi.cs
@@ -14,10 +14,10 @@ namespace OpenWeatherMap
 				Method = HttpMethod.Get,
 				RequestUri = new Uri("https://community-open-weather-map.p.rapidapi.com/find?q=sao%20paulo&cnt=50&mode=null&lon=0&type=link%2C%20accurate&lat=0&units=imperial%2C%20metric"),
 				Headers =
-	{
-		{ "x-rapidapi-key", "6a3a456950msh29fe948fd7e1135p1b03a8jsnd56b25654414" },
-		{ "x-rapidapi-host", "community-open-weather-map.p.rapidapi.com" },
-	},
+				{
+					{ "x-rapidapi-key", "6a3a456950msh29fe948fd7e1135p1b03a8jsnd56b25654414" },
+					{ "x-rapidapi-host", "community-open-weather-map.p.rapidapi.com" },
+				},
 			};
 			using (var response = await client.SendAsync(request))
 			{
